6f6cce30deab7b63e9016c2a03cb4684f7ef0f2d
tim.tg.gerhard <tim.tg.gerhard@googlemail.com>
2011-09-08 23:25:14 +0000

    added some code documentation

--- working/src/browserbridge_server.go
+++ working/src/browserbridge_server.go
@@ -27,9 +27,12 @@ import (
 )
 
 
+// Opens a url in browser
 func OpenURL(url string, browsercommand string) {
-	comm := strings.Replace(browsercommand,"%u",url,1)
+	comm := strings.Replace(browsercommand,"%u",url,1) // replace %u with received url
 	os.Stdout.WriteString("executing:  " + comm + "\n\n")
+   
+  // split command from arguments
 	comms := strings.Split(comm," ")
 	args := ""
 	for i := 1; i < len(comms); i++ {
@@ -38,25 +41,36 @@ func OpenURL(url string, browsercommand string) {
 		}
 		args += comms[i]
 	}
+   
+  // execute
 	cm := exec.Command(comms[0],args)
 	cm.Start()
 }
 
 
+// React on incoming TCP connection
 func HandleClient(conn *net.TCPConn, password string, browsercommand string) {
+
+  // prepare for message, and wait for it
 	mess := make([]byte,1024)
 	conn.Read(mess)
+   
+  // get message
 	message := string(mess)
 	os.Stdout.WriteString("\n\ngot this url:  ")
-	s := strings.Split(message,"\n")
-	url := s[0]
-	if len(s) < 2 {
+	s := strings.Split(message,"\n") // network message convention between server and client: "url\npassword\n"
+   
+  // s should now contain 2 elements. s[0]=url, s[1]=password
+  if len(s) < 2 {
 		os.Stdout.WriteString("ERROR: can't understand client.")
-		conn.Write([]byte("nak"))
+		conn.Write([]byte("nak")) //send nak if url wasn't opened
 		return
 	}
+  url := s[0]
 	os.Stdout.WriteString(url + "\n")
 	pass := strings.Replace(s[1],"\n","",-1)
+   
+  //check password, reply
 	if pass != password {
 		os.Stdout.WriteString("invalid password!\n\n")
 			conn.Write([]byte("nak"))
@@ -65,8 +79,12 @@ func HandleClient(conn *net.TCPConn, password string, browsercommand string) {
 		conn.Write([]byte("ack"))
 		OpenURL(url,browsercommand)
 	}
+  
 	conn.Close()
 }
+
+
+
 
 func main() {
 	copyright_notice.Print()
@@ -74,9 +92,12 @@ func main() {
 	os.Stdout.WriteString("Starting Server Interface...\n")
 	browsercommand, port, password, _ := browserbridge_config.ReadPropertiesFile()
 
+  // start listener
 	tcpad,_ := net.ResolveTCPAddr("tcp",":"+port)
 	list,_ := net.ListenTCP(tcpad.Network(), tcpad)
 	defer list.Close()
+   
+  // enter endless loop, waiting for clients
 	for {
 		os.Stdout.WriteString("waiting for clients on port "+port+"...\n")
 		conn,_ := list.AcceptTCP()
